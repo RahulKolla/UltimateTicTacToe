8e2bb316d0671f38c319313c435f9640cde64e03
Rahul Kolla <56570387+RahulKolla@users.noreply.github.com>
2021-12-07 23:28:25 -0600

    Update Ultimate.java

--- Ultimate.java
+++ Ultimate.java
@@ -4,18 +4,16 @@ public class Ultimate {
         int selection;
         Player player = new Player();
         do {
-   
-            System.out.println("1. AI vs AI ");
-   
-            System.out.println("2. Player vs AI");
-   
-            System.out.println("3. Player vs Player ");
-   
+            System.out.println("1. AI vs AI ")' 
+            System.out.println("2. Player vs AI");   
+            System.out.println("3. Player vs Player ");  
             System.out.print("Please select a game to play: ");
+            
             selection = player.humanInput();
-   
+
             if(selection > 3 || selection < 1)
                 System.out.println("Please select a valid game (1-3)");
+            
         }while(selection < 1 || selection > 3);
         Game game = new Game(selection);
     }
